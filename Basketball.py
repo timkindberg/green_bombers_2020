437b124127fd0a42006d90a812e5870f48411938
greenbombers <76501064+greenbombers@users.noreply.github.com>
2020-12-22 19:10:58 -0500

    Update basketball Move to challenge and back to home.

--- Basketball.py
+++ Basketball.py
@@ -2,22 +2,30 @@ from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, Motio
 from spike.control import wait_for_seconds, wait_until, Timer
 import math
 
-# Basketball Challenge - Jordan
 hub = PrimeHub()
 hub.motion_sensor.reset_yaw_angle()
 motor_pair = MotorPair( 'E','F' )
 motor_pair.set_motor_rotation(8.71 * math.pi,'cm')
-print(hub.motion_sensor.get_yaw_angle())
-motor_pair.move(129, unit='degrees', steering=100, speed=30)
-print(hub.motion_sensor.get_yaw_angle())
-motor = Motor('D')
+raiser = Motor('A')
 
-# motor.run_for_degrees(-1229, 80)
+def turn(stop_angle, speed=7):
+    hub.motion_sensor.reset_yaw_angle()
+    motor_pair.start(speed=speed, steering=100 if stop_angle > 0 else -100)
+    start_angle = current_angle = hub.motion_sensor.get_yaw_angle() + 180
+    while(current_angle < stop_angle + 180 if stop_angle > 0 else current_angle > stop_angle + 180):
+        current_angle = hub.motion_sensor.get_yaw_angle() + 180
+        print("current angle: {}".format(current_angle))
+    motor_pair.stop()   
 
-# motor_pair.move(3,unit='in',steering=0,speed=5) #forward
-# motor_pair.move(20,unit='in',steering=0,speed=25) #forward
-# motor_pair.move(5,unit='in',steering=0,speed=-10) #backward
-# motor_pair.move(25,unit='in',steering=-5,speed=-50) #backward
 
-# ###########
-###########
+
+motor_pair.move(5,unit='in',steering=0,speed=-10) #backward
+motor_pair.move(25,unit='in',steering=0,speed=-30) #backward
+turn(-80)
+motor_pair.move(8,unit='in',steering=0,speed=-30) #backward
+motor_pair.move(3,unit='in',steering=0,speed=-10) #backward
+# raiser.run_for_degrees(-1229, 80)
+motor_pair.move(3,unit='in',steering=0,speed=10) #backward
+motor_pair.move(8,unit='in',steering=0,speed=30) #backward
+turn(80)
+motor_pair.move(30,unit=    'in',steering=0,speed=50) #backward
