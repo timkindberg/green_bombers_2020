a6f131e46f9e0f14dd100b7595203555a19ac841
greenbombers <76501064+greenbombers@users.noreply.github.com>
2021-01-26 20:02:05 -0500

    Added cube_dropper

--- StepCounter.py
+++ StepCounter.py
@@ -1,10 +1,15 @@
 from spike import PrimeHub, LightMatrix, Button, StatusLight, ForceSensor, MotionSensor, Speaker, ColorSensor, App, DistanceSensor, Motor, MotorPair
 from spike.control import wait_for_seconds, wait_until, Timer
+
+hub = PrimeHub()
 import math
+#Zander Mission slot 3
 
 hub = PrimeHub()
 motor_pair = MotorPair('E','F')
 motor_pair.set_motor_rotation(8.71 * math.pi,'cm')
+
+cube_dropper = Motor('A')
 
 def turn(stop_angle, speed=10):
     hub.motion_sensor.reset_yaw_angle()
@@ -15,7 +20,8 @@ def turn(stop_angle, speed=10):
         print("current angle: {}".format(current_angle))
     motor_pair.stop()
 
-def boogie():
+def boogie(): 
+    turn(170)
     while(True):
         hub.speaker.set_volume(100)
         hub.speaker.beep(note=60, seconds=0.2)
@@ -26,32 +32,32 @@ def boogie():
         motor_pair.move(1, unit='in', speed=-50)
         motor_pair.move(1, unit='in', speed=50)
         motor_pair.move(1, unit='in', speed=-50)
-        hub.speaker.start_beep(note=90)
-        turn(160, speed=25)
-        hub.speaker.stop()
-        turn(-30, speed=25)
-        turn(30, speed=25)
-        turn(-30, speed=25)
-        turn(30, speed=25)
-        turn(-3, speed=60)
-        turn(3, speed=60)
-        turn(-3, speed=60)
-        turn(3, speed=60)
-        turn(-3, speed=60)
-        turn(3, speed=60)
+        turn(-15, speed=25)
+        turn(15, speed=25)
+        turn(-15, speed=25)
+        turn(15, speed=25)
+        turn(-3, speed=30)
+        turn(3, speed=30)
+        turn(-3, speed=30)
+        turn(3, speed=30)
+        turn(-3, speed=30)
+        turn(3, speed=30)
 
 
 
 
 
-#Zander Mission slot 3
+# Zander Mission slot 3
 motor_pair.move_tank(45, unit='in', left_speed=-67, right_speed=-70) #30 no attachment
 
-motor_pair.move(1.5, unit='in', speed=20)
+motor_pair.move(2, unit='in', speed=20)
 turn(-90)
 motor_pair.move(1, unit='in', speed=20)
-motor_pair.move(22, unit='in', speed=-50)
-turn(-29)
-motor_pair.move(16.5, unit='in', speed=-50)
+motor_pair.move(29.25, unit='in', speed=-50)
+cube_dropper.run_for_degrees(45)
+wait_for_seconds(2)
+motor_pair.move(7.25, unit='in', speed=50)
+turn(-29) 
+motor_pair.move(16, unit='in', speed=-50)
 
 boogie()
